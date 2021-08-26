5d4e047d2eac7355175a08e3b5f6cb45a779514e
Murilo Pires <murilopires99@gmail.com>
2021-08-26 00:29:05 -0300

    first-screen-design

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -11,6 +11,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        alignment: Alignment.topCenter,
         children: [
           Container(
             height: double.infinity,
@@ -31,27 +32,153 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           Container(
             height: double.infinity,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  // Logo
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.tight,
-                    child: Text(
-                      'Not implemented',
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 40
+                ),
+                child: Column(
+                  children: <Widget>[
+                    // Logo
+                    Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(top: 50),
+                          child: Text(
+                            'S A U R O N',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 160,
+                          padding: EdgeInsets.symmetric(vertical:10),
+                          child: Image.asset('assets/images/sauron.png'),
+                        ),
+                      ],
+                    ),
+                    // Form
+                    SizedBox(height: 50,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(174,97,209,1),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 6.0,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(top: 15),
+                              border: InputBorder.none,
+                              prefixIcon: Icon(
+                                Icons.email, 
+                                color: Colors.white,
+                              ),
+                              hintText: 'Digite seu email',
+                              hintStyle: TextStyle(
+                                color: Colors.white54,
+                              )
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15,),
+                        Text(
+                          'Senha',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(174,97,209,1),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 6.0,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(top: 15),
+                              border: InputBorder.none,
+                              prefixIcon: Icon(
+                                Icons.lock, 
+                                color: Colors.white,
+                              ),
+                              hintText: 'Digite sua senha',
+                              hintStyle: TextStyle(
+                                color: Colors.white54,
+                              )
+                            ),
+                          ),
+                        ),
+                        SizedBox(height:90),
+                        Container(
+                          width: double.infinity,
+                          height: 45,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              elevation: MaterialStateProperty.all(5),
+                              backgroundColor: MaterialStateProperty.all(Colors.white),
+                              padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                              )
+                            ),
+                            child: Text(
+                              'ENTRAR', 
+                              style: TextStyle(
+                                color: Color.fromRGBO(153,28,209,1),
+                                letterSpacing: 1.5
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     )
-                  ),
-                  // Form
-                  Flexible(
-                    flex: 4,
-                    fit: FlexFit.tight,
-                    child: Text('Not implemented')
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-        ],
+          ),
+        ]
       ),
     );
   }
